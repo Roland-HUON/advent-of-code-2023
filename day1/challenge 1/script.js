34ddeccddfe5d9eab59f12bbc598e6e8c819854d
Roland HUON <huonroland91@gmail.com>
2023-12-24 13:51:05 +0100

    ⚡️ if there is one numbers just double it

--- day1/challenge 1/script.js
+++ day1/challenge 1/script.js
@@ -1,11 +1,10 @@
 let element = document.querySelector('body'); // replace 'selector' with your CSS selector
 let content = element.innerHTML; // gets the content of the element
 let lines = content.split('\n'); // splits the content into lines
+let output = [];
 let total=[];
-let total2 = 0;
 for (let line of lines) { // iterates over each line
     let numbers = line.match(/\d+/g) || [];// gets all numbers in the line
-    let output = [];
     var Snumbers = numbers.toString();
     for (var i = 0, len = Snumbers.length; i < len; i += 1) {
         output.push(+Snumbers.charAt(i));
@@ -16,14 +15,19 @@ for (let line of lines) { // iterates over each line
         result = output[0]*10 + output[output.length-1];
         total.push(result);
     } else if (output.length === 1) {// if there is only 1 number
-        result = output[0];
+        result = output[0]*10 + output[0];
         total.push(result);
     } else {// if there are no numbers
-        result = '';
+        result = 0;
         total.push(result);
     }
     console.log(result);
 }
-// console.log(total);
-total.forEach((element)=>total2+=element);
-console.log("Le tout est égale à : "+total2);
+console.log(total);
+for (var i = 0, sum = 0; i < total.length; sum += total[i++]){
+    console.log(sum);
+}
+// total.forEach((element)=>{
+//     total2+=element;
+// });
+// console.log("Le tout est égale à : "+total2);
