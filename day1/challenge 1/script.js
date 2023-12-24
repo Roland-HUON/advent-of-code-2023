f0c1b249056c0567d073ffe4e3c9e2e32a71ccdb
Roland HUON <huonroland91@gmail.com>
2023-12-24 13:01:11 +0100

    ✨ Divided numbers into individuals numbers

--- day1/challenge 1/script.js
+++ day1/challenge 1/script.js
@@ -1,16 +1,29 @@
 let element = document.querySelector('body'); // replace 'selector' with your CSS selector
-let content = element.innerHTML;
-let lines = content.split('\n');
-for (let line of lines) {
-    let numbers = line.match(/\d+/g) || [];
+let content = element.innerHTML; // gets the content of the element
+let lines = content.split('\n'); // splits the content into lines
+let total=[];
+let total2 = 0;
+for (let line of lines) { // iterates over each line
+    let numbers = line.match(/\d+/g) || [];// gets all numbers in the line
+    let output = [];
+    var Snumbers = numbers.toString();
+    for (var i = 0, len = Snumbers.length; i < len; i += 1) {
+        output.push(+Snumbers.charAt(i));
+    }
     let result;
-    if (numbers.length > 1) {
-        console.log(numbers);
-        result = numbers[0] + numbers[numbers.length-1];
-    } else if (numbers.length === 1) {
-        result = numbers[0];
-    } else {
+    if (output.length > 1) {// if there are more than 1 numbers
+        console.log(output);
+        result = output[0]*10 + output[output.length-1];
+        total.push(result);
+    } else if (output.length === 1) {// if there is only 1 number
+        result = output[0];
+        total.push(result);
+    } else {// if there are no numbers
         result = '';
+        total.push(result);
     }
     console.log(result);
 }
+// console.log(total);
+total.forEach((element)=>total2+=element);
+console.log("Le tout est égale à : "+total2);
